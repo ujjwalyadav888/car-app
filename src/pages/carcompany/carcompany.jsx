1e4ce5438742035bb89c4d4885fc414fac157f83
Ujjwal yadav <165245292+ujjwalyadav888@users.noreply.github.com>
2025-06-10 00:53:52 +0530

    Update carcompany.jsx

--- src/pages/carcompany/carcompany.jsx
+++ src/pages/carcompany/carcompany.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './CarCompany.css'; 
+import './carcompany.css'; 
 import { Link } from 'react-router-dom';
 
 const CarCompany = () => {
